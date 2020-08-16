74163a2214fae85984897622196af968a44f9566
Arturo Rendon <arturo@sube.la>
2020-08-16 15:57:22 -0700

    Angular Boilerplate Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AutomataEvaluator-Client';
