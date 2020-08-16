3baefe138af1c61228c667ede1f30b1fa36fceb9
Arturo Rendon <arturo@sube.la>
2020-08-16 15:48:10 -0700

    Initial Changes

---
+++ src/app/Dashboard/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dashboard',
+  template: '<h1>Dashboard</h1>',
+})
+export class Dashboard implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Dashboard } from './Dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: Dashboard },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
