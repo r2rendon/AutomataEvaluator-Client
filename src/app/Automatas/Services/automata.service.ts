31ff20649c41931139888706aaf9b1458b66e482
Arturo rendon <arturo@sube.la>
2020-08-30 14:43:11 -0600

    Added DFA creation in service

--- src/app/Automatas/Services/automata.service.ts
+++ src/app/Automatas/Services/automata.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from "@angular/core"
-import { HttpClient } from "@angular/common/http"
+import { HttpClient, HttpHeaders } from "@angular/common/http"
 
 @Injectable()
 export class AutomataService {
 
     constructor(private http: HttpClient){}
+
+    createAutomata(newAutomata){
+        let options = {
+            headers: new HttpHeaders({ "Content-Type": "application/json" }),
+            responseType: "text" as "json"
+        };
+        if(newAutomata.type == 'dfa'){
+            const alphabet = newAutomata.automataAlphabet.split(',');
+            const states = newAutomata.automataStates.split(',');
+            const acceptingStates = newAutomata.automataAcceptingStates.split(',');
+            const transitionsSplit: Array<String> = newAutomata.automataTransitions.split(';');
+            let transitions = {};
+            transitionsSplit.forEach(transition => {
+                const split = transition.split(',');
+                transitions[`${split[0]}, ${split[1]}`] = split[2]
+            });
+            const automataObj = {
+                "type": newAutomata.type,
+                "alphabet": alphabet,
+                "states": states,
+                "initial_state": newAutomata.automataInitialStates,
+                "accepting_states": acceptingStates,
+                "transitions": transitions
+            }
+
+            return this.http.post(`http://127.0.0.1:5000/automata`, automataObj, options);
+
+        }
+    }
 
 }
