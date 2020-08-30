aef836eae85b53f9a39e7a964d2b300f532bd9d9
Arturo rendon <arturo@sube.la>
2020-08-30 13:19:58 -0600

    Component addition to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Navbar/navbar.component'
 import { CommonModule } from '@angular/common';
+import { Dashboard } from './Dashboard/dashboard.component';
+import { CreateAutomataComponent } from './Automatas/Create/create-automata.component';
+import { DFA_Component } from './Automatas/DFA/dfa.component';
+import { ENFA_Component } from './Automatas/ENFA/enfa.component';
+import { REGEX_Component } from './Automatas/REGEX/regex.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    Dashboard,
+    CreateAutomataComponent,
+    DFA_Component,
+    ENFA_Component,
+    REGEX_Component
   ],
   imports: [
     BrowserModule,
