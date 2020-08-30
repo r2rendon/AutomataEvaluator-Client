1c2fdfc63320055e5306d806a827abde1b8355a0
Arturo rendon <arturo@sube.la>
2020-08-30 03:40:58 -0600

    Automata component addition to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Dashboard } from './Dashboard/dashboard.component';
+import { DFA_Component } from './Automatas/DFA/dfa.component'
+import { ENFA_Component } from './Automatas/ENFA/enfa.component'
+import { REGEX_Component } from './Automatas/REGEX/regex.component'
 
 const routes: Routes = [
   { path: 'dashboard', component: Dashboard },
+  { path: 'automata/dfa', component: DFA_Component},
+  { path: 'automata/enfa', component: ENFA_Component},
+  { path: 'automata/regex', component: REGEX_Component},
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
