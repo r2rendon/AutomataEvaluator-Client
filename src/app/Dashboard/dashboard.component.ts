fe90a3f16f01a416e1851ca7c672f0911b65ded1
Arturo rendon <arturo@sube.la>
2020-08-30 13:28:41 -0600

    Dashboard now uses templateUrl

--- src/app/Dashboard/dashboard.component.ts
+++ src/app/Dashboard/dashboard.component.ts
@@ -3,28 +3,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'dashboard',
-  template: `
-    <div class="container">
-      <div class="jumbotron mt-3">
-          <h1 class="display-4">Hi!</h1>
-          <p class="lead">Welcome to this awesome app, feel free to use this app however you want!</p>
-          <hr class="my-4">
-          <small>So you don't have your own Automata?<a class="nav-link p-0 d-inline" (click)="goToCreateAutomata()"> Click here to create it</a></small>
-      </div>
-
-      <div class="row">
-          <li *ngFor="let item of items" class="col-md-4">
-              <div class="card flex-md-row mb-4 box-shadow h-md-250">
-                  <div class="card-body d-flex flex-column align-items-start">
-                      <h3 class="mb-0">{{item.title}}</h3>
-                      <div class="card-text mb-auto">{{item.content}}</div>
-                      <img class="card-img-right flex-auto d-none d-md-block" src="{{item.img}}">
-                  </div>
-              </div>
-          </li>
-      </div>
-    </div>
-  `,
+  templateUrl: './dashboard.component.html' 
 })
 export class Dashboard implements OnInit {
   items: any
