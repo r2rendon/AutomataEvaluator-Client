1543572ad20d5f13d8db7c1a4e975c193b469d00
Arturo rendon <arturo@sube.la>
2020-08-30 22:55:37 -0600

    Defined ENFA component

--- src/app/Automatas/ENFA/enfa.component.ts
+++ src/app/Automatas/ENFA/enfa.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { AutomataService } from '../Services/automata.service'
+import { Router } from '@angular/router'
 
 @Component({
     selector: 'enfa',
     templateUrl: './enfa.component.html'
 })
 
-export class ENFA_Component {}
+export class ENFA_Component implements OnInit {
+    automatas: any
+
+    constructor(private service: AutomataService, private router: Router){
+
+    }
+
+    ngOnInit(){
+        this.service.getAutomataByType('enfa').subscribe((data: Array<any>) => {
+            this.automatas = data
+        })
+    }
+
+    goToAutomata(id){
+        this.router.navigate([`/automata/detail/${id}`])
+    }
+}
