58c4ff8a971b5c6d107eb5970d345b075ff3af58
Arturo rendon <arturo@sube.la>
2020-08-30 18:50:29 -0600

    Get automatas by type method

--- src/app/Automatas/Services/automata.service.ts
+++ src/app/Automatas/Services/automata.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from "@angular/core"
 import { HttpClient, HttpHeaders } from "@angular/common/http"
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Injectable()
 export class AutomataService {
 
-    constructor(private http: HttpClient){}
+    api = 'http://127.0.0.1:5000/'
+    path_on_cloud = "automataImages/"
+
+    constructor(private http: HttpClient, private storage: AngularFireStorage){}
+
+    getAutomataByType(type){
+        return this.http.get(this.api+`automatas/${type}`)
+    }
 
     createAutomata(newAutomata){
         let options = {
-            headers: new HttpHeaders({ "Content-Type": "application/json" }),
-            responseType: "text" as "json"
+            headers: new HttpHeaders({ "Content-Type": "application/json" })
         };
-        if(newAutomata.type == 'dfa'){
+        if(newAutomata.automataType == 'dfa'){
             const alphabet = newAutomata.automataAlphabet.split(',');
             const states = newAutomata.automataStates.split(',');
             const acceptingStates = newAutomata.automataAcceptingStates.split(',');
@@ -22,16 +29,15 @@ export class AutomataService {
                 transitions[`${split[0]}, ${split[1]}`] = split[2]
             });
             const automataObj = {
-                "type": newAutomata.type,
+                "type": newAutomata.automataType,
+                "name": newAutomata.automataName,
                 "alphabet": alphabet,
                 "states": states,
-                "initial_state": newAutomata.automataInitialStates,
+                "initial_state": newAutomata.automataInitialState,
                 "accepting_states": acceptingStates,
                 "transitions": transitions
             }
-
-            return this.http.post(`http://127.0.0.1:5000/automata`, automataObj, options);
-
+            return this.http.post(this.api+'automata', automataObj, options);
         }
     }
 
